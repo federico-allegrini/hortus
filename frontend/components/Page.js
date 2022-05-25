71f6f3736b82c38c5464c23fa855f1728c39615c
folkdev <folk.development@gmail.com>
2022-05-25 17:47:58 +0200

    Frontend setup

---
+++ frontend/components/Page.js
@@ -0,0 +1,64 @@
+import PropTypes from "prop-types";
+import styled, { createGlobalStyle } from "styled-components";
+import Header from "./Header";
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Akshar';
+    src: url('/static/Akshar.ttf') format('ttf');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    --green: #2e7d32;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite: #ededed;
+    --maxWidth: 1250px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'Akshar', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height:2;
+  }
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+  button {
+    font-family: 'Akshar', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+export default function Page({ children }) {
+  return (
+    <div>
+      <GlobalStyles />
+      <Header />
+      <InnerStyles>{children}</InnerStyles>
+    </div>
+  );
+}
+
+Page.propTypes = {
+  children: PropTypes.any,
+};
