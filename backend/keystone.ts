f29d36fc0ef1af923ae7299bb524bf8d578768c2
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-05-28 23:04:48 +0200

    Backend: user, auth, eslint correction for typescript

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,22 +1,49 @@
 import { config, createSchema } from "@keystone-next/keystone/schema";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+import { createAuth } from "@keystone-next/auth";
+import User from "./schemas/User";
 import "dotenv/config";
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/keystone-hortus-test";
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
-  },
-  db: {
-    adapter: "mongoose",
-    url: databaseURL,
-  },
-  lists: createSchema({}),
-  ui: {
-    isAccessAllowed: () => true,
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
   },
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+    },
+    lists: createSchema({
+      User,
+    }),
+    ui: {
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: "id name email",
+    }),
+  })
+);
